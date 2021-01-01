5fe5b2a75e06a0137b4fa97617fd33ae9de80964
hanifrabbani <56330368+rabbanizhnf@users.noreply.github.com>
2021-01-01 15:58:59 +0700

    ganti tampilan

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<!doctype html>
-<html lang="en" class="fullscreen-bg">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-	<title>document</title>
+    <title>document</title>
+
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -12,78 +13,58 @@
 	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
 	<!-- MAIN CSS -->
 	<link rel="stylesheet" href="assets/css/main.css">
-	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
 	<!-- <link rel="stylesheet" href="assets/css/demo.css"> -->
 	<!-- GOOGLE FONTS -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+
+	<!-- custom css -->
+	<link rel="stylesheet" href="assets/css/login.css">
+
 	<!-- Koneksi -->
 	<?php
 
+	// koneksi ke database
 	include 'admin/konfig.php';
 	
 	?>
-	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
 </head>
 
 <body>
-	<!-- cek login -->
-	<?php
-	if (isset($_GET['pesan'])) {
-		if ($_GET['pesan'] == "Gagal") {
-			echo "<div style='margin-bottom:-55px' class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-warning-sign'></span>  Login Gagal !! Username dan Password Salah !!</div>";
-		}
-	}
-	?>
+    
+  <!-- login -->
+  <section id="login">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-8">
+          <div class="row">
+            <div class="col-sm-3"> </div>
 
-	<!-- WRAPPER -->
-	<div id="wrapper">
-		<div class="vertical-align-wrap">
+            <div class="col-sm-6">
+              <div class="card card-login">
+                <form class="login-form text-center" action="cek_login.php" method="POST">
+                  <h1 class="mb-5 font-weight-normal text-uppercase">Login Admin</h1>
+                  
+                  <?php if (isset($_GET['pesan'])) : ?>
+                    <div class="alert alert-danger font-italic" role="alert">
+                      Username / Password Wrong !
+                    </div>
+                  <?php endif; ?>
+                  <div class="form-group">
+                    <input type="text" name="username" for="username" class="form-control form-control-lg" autofocus autocomplete="off" placeholder="Username" required>
+                  </div>
+                  <div class="form-group">
+                    <input type="password" name="password" for="password" class="form-control form-control-lg" placeholder="Password" required>
+                  </div>
+                  <button type="submit" name="submit" class="btn mt-5 btn-warning btn-custom btn-block text-uppercase rounded-pill btn-lg">
+                    Login
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-			<div class="vertical-align-middle">
-				<div class="auth-box ">
-					<div class="left">
-						<div class="content">
-							<div class="header">
-								<div class="logo text-center"><img src="assets/img/logo-dark.png" alt="Klorofil Logo"></div>
-								<p class="lead">Login to your account</p>
-							</div>
-							<form class="form-auth-small" action="cek_login.php" method="post">
-								<div class="form-group">
-									<label for="signin-email" class="control-label sr-only">Username</label>
-									<input type="text" class="form-control" name="username" placeholder="Username">
-								</div>
-								<div class="form-group">
-									<label for="signin-password" class="control-label sr-only">Password</label>
-									<input type="password" class="form-control" name="password" placeholder="Password">
-								</div>
-								<div class="form-group clearfix">
-									<label class="fancy-checkbox element-left">
-										<input type="checkbox">
-										<span>Remember me</span>
-									</label>
-								</div>
-								<button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
-								<div class="bottom">
-									<span class="helper-text"><i class="fa fa-lock"></i> <a href="#">Forgot password?</a></span>
-								</div>
-							</form>
-						</div>
-					</div>
-					<div class="right">
-						<div class="overlay"></div>
-						<div class="content text">
-							<h1 class="heading">Free Bootstrap dashboard template</h1>
-							<p>by The Develovers</p>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- END WRAPPER -->
-</body>
-
-</html>
+  
