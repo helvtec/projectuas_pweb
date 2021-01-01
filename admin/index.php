0fc6be8a78f5835ea335c51decd71dbfe40871f6
hanifrabbani <56330368+rabbanizhnf@users.noreply.github.com>
2021-01-01 16:57:07 +0700

    add total transaksi

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+	
 <?php 
 	session_start();
 	include 'konfig.php';
@@ -76,257 +77,32 @@
 			<!-- MAIN CONTENT -->
 			<div class="main-content">
 				<div class="container-fluid">
-					<h3 class="page-title">Tables</h3>
-					<div class="row">
-						<div class="col-md-6">
-							<!-- BASIC TABLE -->
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Basic Table</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>First Name</th>
-												<th>Last Name</th>
-												<th>Username</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Steve</td>
-												<td>Jobs</td>
-												<td>@steve</td>
-											</tr>
-											<tr>
-												<td>2</td>
-												<td>Simon</td>
-												<td>Philips</td>
-												<td>@simon</td>
-											</tr>
-											<tr>
-												<td>3</td>
-												<td>Jane</td>
-												<td>Doe</td>
-												<td>@jane</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-							<!-- END BASIC TABLE -->
+
+
+					<div class="panel panel-headline">
+						<div class="panel-heading">
+							<h3 class="panel-title">Penjualan Buku</h3>
+							<p class="panel-subtitle"></p>
 						</div>
-						<div class="col-md-6">
-							<!-- TABLE NO PADDING -->
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Table Inside No Padding</h3>
+						<div class="panel-body">
+							<div class="row">
+								<?php
+								$query = "SELECT SUM(total) as total from head_transaksi";
+								$result = mysqli_query($koneksi, $query);
+
+								while ($row = mysqli_fetch_array($result)) {
+								?>
+								<div class="col-md-4">
+									<div class="metric">
+										<span class="icon"><i class="fa fa-money"></i></span>
+										<p>
+											<span class="number"><?php echo "Rp " .rupiah($row['total']) ?></span>
+											<span class="title">Total Nominal Transaksi</span>
+										</p>
+									</div>
 								</div>
-								<div class="panel-body no-padding">
-									<table class="table">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>First Name</th>
-												<th>Last Name</th>
-												<th>Username</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Steve</td>
-												<td>Jobs</td>
-												<td>@steve</td>
-											</tr>
-											<tr>
-												<td>2</td>
-												<td>Simon</td>
-												<td>Philips</td>
-												<td>@simon</td>
-											</tr>
-											<tr>
-												<td>3</td>
-												<td>Jane</td>
-												<td>Doe</td>
-												<td>@jane</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
+								
 							</div>
-							<!-- END TABLE NO PADDING -->
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6">
-							<!-- TABLE STRIPED -->
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Striped Row</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table table-striped">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>First Name</th>
-												<th>Last Name</th>
-												<th>Username</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Steve</td>
-												<td>Jobs</td>
-												<td>@steve</td>
-											</tr>
-											<tr>
-												<td>2</td>
-												<td>Simon</td>
-												<td>Philips</td>
-												<td>@simon</td>
-											</tr>
-											<tr>
-												<td>3</td>
-												<td>Jane</td>
-												<td>Doe</td>
-												<td>@jane</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-							<!-- END TABLE STRIPED -->
-						</div>
-						<div class="col-md-6">
-							<!-- TABLE HOVER -->
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Hover Row</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table table-hover">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>First Name</th>
-												<th>Last Name</th>
-												<th>Username</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Steve</td>
-												<td>Jobs</td>
-												<td>@steve</td>
-											</tr>
-											<tr>
-												<td>2</td>
-												<td>Simon</td>
-												<td>Philips</td>
-												<td>@simon</td>
-											</tr>
-											<tr>
-												<td>3</td>
-												<td>Jane</td>
-												<td>Doe</td>
-												<td>@jane</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-							<!-- END TABLE HOVER -->
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6">
-							<!-- BORDERED TABLE -->
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Bordered Table</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table table-bordered">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>First Name</th>
-												<th>Last Name</th>
-												<th>Username</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Steve</td>
-												<td>Jobs</td>
-												<td>@steve</td>
-											</tr>
-											<tr>
-												<td>2</td>
-												<td>Simon</td>
-												<td>Philips</td>
-												<td>@simon</td>
-											</tr>
-											<tr>
-												<td>3</td>
-												<td>Jane</td>
-												<td>Doe</td>
-												<td>@jane</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-							<!-- END BORDERED TABLE -->
-						</div>
-						<div class="col-md-6">
-							<!-- CONDENSED TABLE -->
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Condensed Table</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table table-condensed">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>First Name</th>
-												<th>Last Name</th>
-												<th>Username</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Steve</td>
-												<td>Jobs</td>
-												<td>@steve</td>
-											</tr>
-											<tr>
-												<td>2</td>
-												<td>Simon</td>
-												<td>Philips</td>
-												<td>@simon</td>
-											</tr>
-											<tr>
-												<td>3</td>
-												<td>Jane</td>
-												<td>Doe</td>
-												<td>@jane</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-							<!-- END CONDENSED TABLE -->
 						</div>
 					</div>
 				</div>
